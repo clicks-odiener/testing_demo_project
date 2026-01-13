1f6c0dff80658d269ae0244e823508bc7fb61bad
Oliver Diener <o.diener@clicks.digital>
2026-01-13 15:16:01 +0100

    Add developer enum

---
+++ src/Enum/DeveloperEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enum;
+
+enum DeveloperEnum: string
+{
+    case FRANZ_BAUMBLEICH = 'Franz Baumbleich';
+    case LYSANDRA_FROSTIG = 'Lysandra Frostig';
+    case OLAF_BUTLEROWSKI = 'Olaf Butlerowski';
+    case RICHARD_TOPFMANN = 'Richard Topfmann';
+    case STEFFEN_HAMMERMANN = 'Steffen Hammermann';
+}
