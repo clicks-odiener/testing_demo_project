cafc0347c02c9184add5963b6a706a1d07eb6ce3
Oliver Diener <o.diener@clicks.digital>
2026-01-22 13:11:55 +0100

    Test

--- tests/AppTest/Unit/A_Simple/A_FirstTest.php
+++ tests/AppTest/Unit/A_Simple/A_FirstTest.php
@@ -10,7 +10,7 @@ class A_FirstTest extends TestCase
 {
     public function testSuccess(): void
     {
-        $this->assertSame(true, true);
+        $this->assertSame(true, false);
     }
 
     public function testFail(): void
